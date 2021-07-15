56e95836f452c9398aa77e1ebf12fa2cdf162108
SoleneMG <solene.michelgrosjean@gmail.com>
2021-07-15 13:54:23 +0200

    change layout root in activity_register.xml add getServer in Inject change order French and English in Spinner

--- app/src/main/java/com/example/cookingv2/Inject.java
+++ app/src/main/java/com/example/cookingv2/Inject.java
@@ -2,12 +2,18 @@ package com.example.cookingv2;
 
 import com.example.cookingv2.data.database.CookingDatabase;
 import com.example.cookingv2.data.database.impl.RoomImpl;
+import com.example.cookingv2.data.server.CookingServer;
+import com.example.cookingv2.data.server.retrofitImpl.RetrofitImpl;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class Inject {
 
+    private static final CookingServer SERVER = new RetrofitImpl();
     private static final CookingDatabase DATABASE = CookingDatabase.getDatabase(MyApplication.getAppContext());
     private static final int numberOfThread = 4;
     private static final ExecutorService EXECUTOR = Executors.newFixedThreadPool(numberOfThread);
@@ -16,4 +22,6 @@ public class Inject {
     public static CookingDatabase getDatabase(){return DATABASE;}
 
     public static ExecutorService getExecutor(){return EXECUTOR;}
+
+    public static CookingServer getServer(){return SERVER;}
 }
